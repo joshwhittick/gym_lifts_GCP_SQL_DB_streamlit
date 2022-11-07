4f21cebfdbc9150630887e26feee06886e0c40b3
Josh Whittick <92024297+joshwhittick@users.noreply.github.com>
2022-11-07 10:59:35 +0000

    Update DB_SQL_STREAMLIT_FUNCTIONS.py

--- DB_SQL_STREAMLIT_FUNCTIONS.py
+++ DB_SQL_STREAMLIT_FUNCTIONS.py
@@ -15,9 +15,9 @@ config = {
     'password': st.secrets["password"],
     'host': st.secrets["host"],
     'client_flags': [ClientFlag.SSL],
-    'ssl_ca': ${{ secrets.SERVER_CA }},
-    'ssl_cert': ${{ secrets.CLIENT_CERT }},
-    'ssl_key': ${{ secrets.CLINET_KEY }},
+    'ssl_ca': os.jw['SERVER_CA'],
+    'ssl_cert': os.jw['CLIENT_CERT'],
+    'ssl_key': os.jw['CLINET_KEY'],
     'database': st.secrets["database"]
 }
 
